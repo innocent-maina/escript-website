360cdda11fd5aaa5e987b3f20558333b6e2521e6
innocent-maina <innocentwahome@gmail.com>
2024-12-17 14:32:13 +0300

    Change naming from Martex

--- components/sections/Demo15/Section18.vue
+++ components/sections/Demo15/Section18.vue
@@ -9,7 +9,7 @@
                         <div class="col-md-8">
                             <div class="banner-7-txt text-center">
                                 <!-- Title -->
-                                <h2 class="s-50 w-700">Starting with Martex is easy, fast and free</h2>
+                                <h2 class="s-50 w-700">Starting with Escript is easy, fast and free</h2>
                                 <!-- Button -->
                                 <NuxtLink to="/signup-1" class="btn r-04 btn--theme hover--tra-black">Get srarted - it's free </NuxtLink>
                                 <!-- Button Text -->
